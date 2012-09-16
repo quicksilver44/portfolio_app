45c112d40d6f4583043aed1f3f5bd1505c70090d
unknown <Administrator@celab1-14.comp1-domain.spit.ac.in>
2012-09-16 14:57:15 +0530

    symbol validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,8 @@
 class Company < ActiveRecord::Base
+  validate :symbol_should_be_less_than_3_chars
+  def symbol_should_be_less_than_3_chars
+    if symbol.length > 4
+      errors.add :symbol,'should be less than 4 chars'
+    end
+  end
 end
